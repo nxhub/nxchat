6482235338c92627ef2e3e27b8f3eec3e615c1c9
codeping <deping.du@gmail.com>
2020-07-12 18:02:48 +0800

    添加 chat 页面。

--- src/NXChat/Startup.cs
+++ src/NXChat/Startup.cs
@@ -19,13 +19,11 @@ namespace NXChat
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
